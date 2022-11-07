cc20ffe2de273cf71f24472a2ebe7a36a1460302
Masha Titova <masha.titova32@gmail.com>
2022-11-07 19:29:53 +0200

    widget

--- js/script.js
+++ js/script.js
@@ -15,3 +15,20 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
+    });
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
